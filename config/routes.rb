1a5dc27f79e7e0d2be5af43d6d53e605c2de39db
Derine Banis <d.banis.13@gmail.com>
2022-07-27 21:38:08 -0700

    Redo setup for about controller and link to about page from bottom nav.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -11,6 +10,8 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+
+  resources :about, only: [:index]
 
   namespace :admin do
     root to: 'dashboard#show'
