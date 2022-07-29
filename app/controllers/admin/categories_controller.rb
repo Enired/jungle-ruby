91b47a681204e73bd3dbbd4d6b5b7f4e0d1e0a64
Derine Banis <d.banis.13@gmail.com>
2022-07-28 17:09:22 -0700

    Create category page for admin. Also create ability to add new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,34 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+
+
+  # def destroy
+  #   @category = category.find params[:id]
+  #   @category.destroy
+  #   redirect_to [:admin, :products], notice: 'Product deleted!'
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :categories, :products, except: [:edit, :update, :show]
+    resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:new, :index, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
