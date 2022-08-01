785dabb767745d74445710227d6492cf39b83842
Derine Banis <d.banis.13@gmail.com>
2022-08-01 16:56:04 -0700

    Add test for proper validation when all fields filled out. Add test for proper validation when name is nil

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'validations' do
-    
+    it 'should save when name, price, quantity, and category are specificed' do
+      category = Category.new
+      product = Product.new(name: 'Some Plant', price:2003, quantity: 3, category: category)
+      product.save
+      expect(product.errors.full_messages).to match_array([])
+    end
+
+    it 'should return "Name can\'t be blank when nil is given' do
+      category = Category.new
+      product = Product.new(name: nil, price:2003, quantity: 3, category: category)
+      product.save
+      expect(product.errors.full_messages).to include('Name can\'t be blank')
+    end
   end
 end
