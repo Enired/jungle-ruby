0a05e83118553d0ea6d72857b4623b9be2780bdf
Derine Banis <d.banis.13@gmail.com>
2022-08-01 16:57:53 -0700

    Rewrite description for invalid name validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,11 +9,17 @@ RSpec.describe Product, type: :model do
       expect(product.errors.full_messages).to match_array([])
     end
 
-    it 'should return "Name can\'t be blank when nil is given' do
+    it 'should not save with invalid name' do
       category = Category.new
       product = Product.new(name: nil, price:2003, quantity: 3, category: category)
       product.save
       expect(product.errors.full_messages).to include('Name can\'t be blank')
     end
+
+   
+
+
+
+
   end
 end
