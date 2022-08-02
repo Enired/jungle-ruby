382664992bd4cf8c4cc41f6908158b25c5370c33
Derine Banis <d.banis.13@gmail.com>
2022-08-02 13:35:55 -0700

    Add test for showing at least 2 products on page.

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -7,4 +7,8 @@ describe('testing home page', () => {
     cy.get(".products article").should('be.visible');
   });
 
+  it('should show at least 2 products on the home page', () => {
+    cy.get(".products article").should('have.length', 2);
+  });
+
 });
