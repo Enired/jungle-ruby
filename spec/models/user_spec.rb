0a9ca8008de0e0a676de6cb6ca4dd95abd0324d0
Derine Banis <d.banis.13@gmail.com>
2022-08-01 18:19:40 -0700

    Add test for password length validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,8 +12,14 @@ RSpec.describe User, type: :model do
     it 'should not save when the password and password confirmation are not the same' do
       user = User.new(first_name: "Eugene", last_name: "Krabs", email: "money@money.com", password: 'hello', password_confirmation: 'jesus')
       user.save
-      p user.errors.full_messages #TESTING PURPOSES
       expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    
+    it 'should not save when the password length is less than 4' do
+      user = User.new(first_name: "Eugene", last_name: "Krabs", email: "money1@money1.com", password: 'hi', password_confirmation: 'hi')
+      user.save
+      p user.errors.full_messages #TESTING PURPOSES
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 4 characters)")
     end
 
   end
