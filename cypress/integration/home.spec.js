7d6cbffc31fed641926316fb5111b7fdbd0ce2d9
Derine Banis <d.banis.13@gmail.com>
2022-08-02 13:01:26 -0700

    Complete test for home page product display

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,10 @@
-describe('testing home page', ()=> {
-  it('visits the home page', ()=> {
-    cy.visit('/')
-  })
+describe('testing home page', () => {
+  it('visits the home page', () => {
+    cy.visit('/');
+  });
 
-})
+  it('should have products on the home page', () => {
+    cy.get(".products article").should('be.visible');
+  });
+
+});
