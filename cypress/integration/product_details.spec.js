35571d5aea4ee870e6980eab667fc018380f7f01
Derine Banis <d.banis.13@gmail.com>
2022-08-02 14:15:35 -0700

    Add test for clicking on second product to go to the product page

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,7 +1,14 @@
 describe('product details page', ()=>{
-  it('should visit the home page then go to the details of the first project.', ()=>{
+  beforeEach(()=>{
     cy.visit('/')
+  })
+  it('should visit the home page then go to the details of the first product.', ()=>{
     cy.get('.products article').first().click()
+    cy.get('.product-detail').should('exist')
+  })
+
+  it('should visit the home page then go to the details of the second product', ()=>{
+    cy.get('.products article').eq(1).click()
     cy.get('.product-detail').should('exist')
   })
 
