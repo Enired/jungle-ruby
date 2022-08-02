80b7868d27fd87b33e010546e3436611ce71dc80
Derine Banis <d.banis.13@gmail.com>
2022-08-01 18:24:06 -0700

    Uncomment out test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    # it 'should not save without a password' do
-    #   user = User.new(first_name: 'Jack', last_name: 'Frost', email: 'jf@jf.com')
-    #   user.save
-    #   p user.errors.full_messages
-    #   expect(user.errors.full_messages).to include("Password can't be blank")
-    # end
+    it 'should not save without a password' do
+      user = User.new(first_name: 'Jack', last_name: 'Frost', email: 'jf@jf.com')
+      user.save
+      p user.errors.full_messages
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
 
     it 'should not save when the password and password confirmation are not the same' do
       user = User.new(first_name: "Eugene", last_name: "Krabs", email: "money@money.com", password: 'hello', password_confirmation: 'jesus')
