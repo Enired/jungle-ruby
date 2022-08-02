46f6aeacd3b7c23fe4397e342b1010fdcfafa706
Derine Banis <d.banis.13@gmail.com>
2022-08-01 18:02:08 -0700

    Add validators to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
+    # it 'should not save without a password' do
+    #   user = User.new(first_name: 'Jack', last_name: 'Frost', email: 'jf@jf.com')
+    #   user.save
+    #   p user.errors.full_messages
+    #   expect(user.errors.full_messages).to include("Password can't be blank")
+    # end
 
+    it 'should save when all fields are filled out' do
+      user = User.new(first_name: "Eugene", last_name: "Krabs", email: "money@money.com", password_digest: 'hello')
+      user.save
+      p user.errors.full_messages
+
+    end
 
   end
 end
